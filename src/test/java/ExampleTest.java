ad019e71c246de5caa9dc2ac81e9216672ffaa39
Christoph Neuroth <christoph.neuroth@gmail.com>
2021-06-01 08:58:35 +0200

    Add mockito example

--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -1,6 +1,13 @@
-import net.jqwik.api.*;
+import net.jqwik.api.Example;
+import net.jqwik.api.ForAll;
+import net.jqwik.api.Property;
+
+import java.util.List;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class ExampleTest {
     @Example
@@ -10,14 +17,22 @@ public class ExampleTest {
 
     @Property
     void additionIsAssociative(@ForAll int a, @ForAll int b) {
-        assertThat(a+b, equalTo(b+a));
+        assertThat(a + b, equalTo(b + a));
     }
 
     @Example
     void testJava15RecordsPointExample() {
-        record Point(int x, int y) { }
+        record Point(int x, int y) {
+        }
 
-        assertThat(new Point(1,2), is(equalTo(new Point(1,2))));
-        assertThat(new Point(1,2), is(not(equalTo(new Point(2,1)))));
+        assertThat(new Point(1, 2), is(equalTo(new Point(1, 2))));
+        assertThat(new Point(1, 2), is(not(equalTo(new Point(2, 1)))));
+    }
+
+    @Example
+    void testMockito() {
+        List mockedList = mock(List.class);
+        mockedList.add("one");
+        verify(mockedList).add("one");
     }
 }

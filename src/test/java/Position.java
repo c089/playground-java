60d4da350e353f65066609ed484319f4da1b6688
Christoph Neuroth <christoph.neuroth@gmail.com>
2022-05-26 11:52:47 +0200

    fix position enum oopsie

--- src/test/java/Position.java
+++ src/test/java/Position.java
@@ -3,7 +3,7 @@ import java.util.stream.Stream;
 
 public enum Position {
     TOP_LEFT, TOP_CENTER, TOP_RIGHT,
-    MIDDLE_LEFT, MIDDLE,CENTER, MIDDLE_RIGHT,
+    MIDDLE_LEFT, MIDDLE_CENTER, MIDDLE_RIGHT,
     BOTTOM_LEFT, BOTTOM_CENTER, BOTTOM_RIGHT;
 
     static Stream<Position> allPositionsExcept(Position position) {

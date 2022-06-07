a8c8da0e068b02d851735cc784350edfb9077b5c
Christoph Neuroth <christoph.neuroth@gmail.com>
2022-06-07 21:09:36 +0200

    fix flaky spark integration test by using awaitInitialization 🥺

--- src/test/java/infrastructure/http/spark/SparkHTTPServerAdapterTest.java
+++ src/test/java/infrastructure/http/spark/SparkHTTPServerAdapterTest.java
@@ -4,8 +4,7 @@ import infrastructure.http.HTTPServerAdapterContract;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
-import static spark.Spark.awaitStop;
-import static spark.Spark.stop;
+import static spark.Spark.*;
 
 public class SparkHTTPServerAdapterTest extends HTTPServerAdapterContract {
 
@@ -13,6 +12,7 @@ public class SparkHTTPServerAdapterTest extends HTTPServerAdapterContract {
     void setUp() {
         SparkHTTPAdapter sparkHTTPAdapter = new SparkHTTPAdapter(useCase);
         sparkHTTPAdapter.start();
+        awaitInitialization();
     }
 
     @AfterEach
